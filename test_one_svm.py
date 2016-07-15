31521379186e684516bdefa5e7c7ec34e2334dd6
elaeon <mara80@gmail.com>
2016-07-15 15:36:52 -0500

    teste script

--- test_one_svm.py
+++ test_one_svm.py
@@ -5,8 +5,10 @@ from skimage import io
 from skimage import exposure
 from sklearn import preprocessing
 
+from ml.processing import Preprocessing
+
 settings = get_settings()
-dataset_name = "numeraiv2"
+dataset_name = "numeraiv3"
 dataset_path = "/home/sc/"
 IMAGE_SIZE = int(settings["image_size"])
 check_point_path = "/home/sc/ml_data/checkpoints/"
@@ -47,17 +49,16 @@ def test():
         io.imsave("/tmp/img2/{}.jpg".format(count), data_n)
         #break
 
-def test_train(dataset, train=False):
-    #classif = ml.clf.SVCFace(dataset, check_point_path=check_point_path, pprint=False)
+def test_train(classif, train=False, num_steps=10):
+    #classif = ml.clf.SVC(dataset, check_point_path=check_point_path, pprint=False)
     #classif = ml.clf.Binary(dataset, check_point_path=check_point_path, pprint=False)
-    classif = ml.clf.TfLTensor(dataset, check_point_path=check_point_path, pprint=False)
+    #classif = ml.clf_e.MLP(dataset, check_point_path=check_point_path, pprint=False)
     #classif = ml.clf.RandomForest(dataset, check_point_path=check_point_path, pprint=False)
-    #classif = ml.clf.LogisticRegression(dataset, check_point_path=check_point_path, pprint=False)
+    #classif = ml.clf_e.LogisticRegression(dataset, check_point_path=check_point_path, pprint=False)
 
     if train is True:
-        classif.train(batch_size=120, num_steps=50)
-    dt = ml.clf.ClassifTest(logloss=True)
-    dt.classif_test(classif, "f1")
+        classif.train(batch_size=128, num_steps=num_steps)
+    classif.print_score()
     #classifs = {
     #    "SCV": {"name": ml.clf.SVCFace, 
     #        "params": {"check_point_path": check_point_path, "pprint":False}}, 
@@ -67,19 +68,17 @@ def test_train(dataset, train=False):
     #        "params": {"check_point_path": check_point_path, "pprint":False}}}
     #dt.dataset_test(classifs, dataset, "f1")
 
-def predict(dataset, path, label_column):
+def predict(classif, path, label_column):
     import pandas as pd
     import csv
-    #classif = ml.clf.SVCFace(dataset, check_point_path=check_point_path, pprint=False)
-    classif = ml.clf.TfLTensor(dataset, check_point_path=check_point_path, pprint=False)
-    #classif = ml.clf.RandomForest(dataset, check_point_path=check_point_path, pprint=False)
+
     df = pd.read_csv(path)
-    dataset = df.drop([label_column], axis=1).as_matrix()
+    dataset_p = df.drop([label_column], axis=1).as_matrix()
+    data = dataset_p
+
     ids = df[label_column].as_matrix()
-    #for predic, label in zip(list(classif.predict(dataset))[:20], ids[:20]):
-    #    print(predic, label)
     predictions = []
-    for value, label in zip(list(classif.predict(dataset, raw=True)), ids):
+    for value, label in zip(list(classif.predict(data, raw=True)), ids):
         predictions.append([str(label), str(value[1])])
         #break
     
@@ -89,44 +88,12 @@ def predict(dataset, path, label_column):
         for row in predictions:
             csvwriter.writerow(row)
 
-def pca(dataset):
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.decomposition import PCA, RandomizedPCA, KernelPCA
-    #reg = LinearRegression()
-    #train_dataset, train_labels = dataset.train_dataset, dataset.train_labels
-    #values = []
-    #for n_components in range(3, 22):
-    #    pca = PCA(n_components=n_components)
-    #    train_dataset_ = pca.fit_transform(train_dataset)
-    #    print(train_dataset_.shape)
-    #    reg = LogisticRegression(solver="lbfgs", multi_class="multinomial")#"newton-cg")
-    #    reg = reg.fit(train_dataset_, train_labels)
-    #    score = reg.score(train_dataset_, train_labels)
-    #    print(score)
-    #    values.append((score, n_components))
-    #p, c = max(values, key=lambda x: x[0])
-    #pca = PCA(n_components=c)
-    c = 20
-    n_dataset = ml.ds.DataSetBuilder(dataset.name)
-    n_dataset.train_dataset = PCA(n_components=c).fit_transform(dataset.train_dataset)
-    n_dataset.test_dataset = PCA(n_components=c).fit_transform(dataset.test_dataset)
-    n_dataset.valid_dataset = PCA(n_components=c).fit_transform(dataset.valid_dataset)
-    n_dataset.train_labels = dataset.train_labels
-    n_dataset.test_labels = dataset.test_labels
-    n_dataset.valid_labels = dataset.valid_labels
-    return n_dataset
-    #predictions = reg.predict(train_dataset)
-
-    ### your code goes here
-    #limit = 10
-    #print(train_labels[:limit])
-    #print(predictions[:limit])
-
 def graph(dataset):
     import numpy as np
     import seaborn as sns
+    import matplotlib.pyplot as plt
     sns.set(color_codes=True)
-    classif = ml.clf.TfLTensor(dataset, check_point_path=check_point_path, pprint=False)
+    classif = ml.clf_e.MLP(dataset, check_point_path=check_point_path, pprint=False)
     dataset_e, _, labels_e = classif.erroneous_clf()
     dataset_c, _, labels_c = classif.correct_clf()
     df_e = ml.ds.DataSetBuilder.to_DF(dataset_e, labels_e)
@@ -138,34 +105,61 @@ def graph(dataset):
     #sns.pairplot(dataset.dataset);
     #sns.distplot(df[df[21] == 1][0])
 
-    e0 = df_e[df_e["target"] == 1].sample(frac=0.1, replace=True).drop(["target"], axis=1).as_matrix()
+    e0 = df_e[df_e["target"] == 1].sample(frac=0.1, replace=True).drop(["target"], axis=1).head(2000).as_matrix()
     #e1 = df_e[df_e["target"] == 0].sample(frac=0.03, replace=True).drop(["target"], axis=1).as_matrix()
-    c0 = df_c[df_c["target"] == 0].sample(frac=0.1, replace=True).drop(["target"], axis=1).as_matrix()
-    #c1 = df_c[df_c["target"] == 1].sample(frac=0.1, replace=True).drop(["target"], axis=1).as_matrix()
+    c0 = df_c[df_c["target"] == 0].sample(frac=0.1, replace=True).drop(["target"], axis=1).head(2000).as_matrix()
+    c1 = df_c[df_c["target"] == 1].sample(frac=0.1, replace=True).drop(["target"], axis=1).as_matrix()
     #c_G = df_G[df_G["target"] == 1].sample(frac=0.01, replace=True).drop(["target"], axis=1).as_matrix()
-    #sns.tsplot(e0, color="r", err_style="boot_traces")
+    #sns.tsplot(e0, color="r")#, err_style="boot_traces")
     #sns.tsplot(e1, color="b", err_style="boot_traces")
-    #sns.tsplot(c0, color="g", err_style="boot_traces")
+    #sns.tsplot(c0, color="g")#, err_style="boot_traces")
     #sns.tsplot(c1, color="m", err_style="boot_traces")
     #sns.tsplot(c_G, color="y", err_style="boot_traces")
-    sns.kdeplot(df_e[df_e["target"] == 1]["c0"], color="r", shade=False)
-    sns.kdeplot(df_c[df_c["target"] == 0]["c0"], color="g", shade=False)
+    #sns.kdeplot(df_e[df_e["target"] == 1]["c0"], color="r", shade=False)
+    #sns.kdeplot(df_c[df_c["target"] == 0]["c0"], color="g", shade=False)
+    #sns.plt.show()
+
+    cr0 = np.correlate(c0[0], c0[0], mode='full')
+    cr0 = cr0[cr0.size/2:]
+    cr1 = np.correlate(c1[0], c1[0], mode='full')
+    cr1 = cr1[cr1.size/2:]
+    er0 = np.correlate(e0[0], e0[0], mode='full')
+    er0 = er0[er0.size/2:]
+    print(np.dot(cr0,er0))
+    print(np.dot(cr1,er0))
+
+
+def normal():
+    import pandas as pd
+    import seaborn as sns
+    df = pd.read_csv("/home/sc/test_data/numerai_datasets/numerai_training_data.csv")
+    df2 = df.drop(["target"], axis=1)
+    df["total"] = df2.sum(axis=1)
+    sns.kdeplot(df[df["target"] == 1]["total"], label="1")
+    sns.kdeplot(df[df["target"] == 0]["total"], label="0")
     sns.plt.show()
+
 
 #build()
 #matrix()
-#dataset = ml.ds.DataSetBuilderFile(dataset_name, dataset_path=dataset_path)
-#dataset.build_dataset("/home/sc/test_data/numerai_datasets/numerai_training_data.csv", "target")
+dataset = ml.ds.DataSetBuilderFile(dataset_name, dataset_path=dataset_path, processing_class=Preprocessing)
+dataset.build_dataset("/home/sc/test_data/numerai_datasets/numerai_training_data.csv", "target")
 """
          0        1
 --  ------  -------
  0  64.991  39.8907
  1  40.42   64.5937
 """
-dataset = ml.ds.DataSetBuilderFile.load_dataset(dataset_name, dataset_path=dataset_path)
-#n_dataset = pca(dataset)
-#outlier(dataset)
-#test_train(dataset, train=True)
-graph(dataset)
-#predict(dataset, "/home/sc/test_data/numerai_datasets/numerai_tournament_data.csv", "t_id")
-
+#dataset = ml.ds.DataSetBuilderFile.load_dataset(dataset_name, dataset_path=dataset_path, processing_class=Preprocessing)
+classif = ml.clf_e.LSTM(dataset, check_point_path=check_point_path, pprint=False, timesteps=7)
+#classif = ml.clf_e.MLP(dataset, check_point_path=check_point_path, pprint=False)
+test_train(classif, train=True, num_steps=10)
+#graph(dataset)
+#normal()
+import pandas as pd
+v,l = dataset.new_validation_dataset(pd.read_csv("/home/sc/test_data/numerai_datasets/numerai_tournament_data.csv"), pd.read_csv("/home/sc/test_data/numerai_datasets/example_predictions.csv"), pd.read_csv("/home/sc/predictions.csv"), "t_id")
+dataset.valid_data = v
+dataset.valid_labels = l
+classif = ml.clf_e.LSTM(dataset, check_point_path=check_point_path, pprint=False, timesteps=7)
+test_train(classif, train=True, num_steps=5)
+predict(classif, "/home/sc/test_data/numerai_datasets/numerai_tournament_data.csv", "t_id")
