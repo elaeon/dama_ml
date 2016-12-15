1ba94965b7879c35fef86cda5200349cfdf740d0
elaeon <mara80@gmail.com>
2016-12-15 14:40:12 -0600

    preload model to keras class

--- src/ml/clf/extended.py
+++ src/ml/clf/extended.py
@@ -220,6 +220,55 @@ class MLP(TFL):
             self.save_model()
 
 
+class FCNet(Keras):
+    def load_fn(self, path):
+        from keras.models import load_model
+        self.net_model = load_model(path)
+        self.model = MLModel(fit_fn=self.net_model.fit, 
+                            predictors=[self.net_model.predict],
+                            load_fn=self.load_fn,
+                            save_fn=self.save_fn)
+
+    def save_fn(self, path):
+        self.net_model.save(path)
+
+    def preload_model(self):
+        self.model = MLModel(fit_fn=None, 
+                            predictors=None,
+                            load_fn=self.load_fn,
+                            save_fn=self.save_fn)
+
+    def prepare_model(self):
+        from keras.layers import Input, Dense
+        from keras.models import Model
+
+        self.layers = [128, 64]
+        input_layer = Input(shape=(self.num_features,))
+        layer_ = input_layer
+        for layer_size in self.layers:
+            dense = Dense(layer_size, activation='tanh', 
+                activity_regularizer=regularizers.activity_l2(10e-5))(input_layer)
+            layer_ = Dropout(0.5)(dense)
+
+        softmax = Dense(self.num_labels, activation='softmax')(layer_)
+        self.net_model = Model(input=input_layer, output=softmax)
+        self.net_model.compile(optimizer='sgd', loss='categorical_crossentropy')
+        self.model = MLModel(fit_fn=self.net_model.fit, 
+                            predictors=[self.net_model.predict],
+                            load_fn=self.load_fn,
+                            save_fn=self.save_fn)
+
+    def train(self, batch_size=258, num_steps=50):
+        self.prepare_model()
+        self.model.fit(self.dataset.train_data, 
+            self.dataset.train_labels,
+            nb_epoch=num_steps,
+            batch_size=batch_size,
+            shuffle=True,
+            validation_data=(self.dataset.valid_data, self.dataset.valid_labels))
+        self.save_model()
+
+
 class ConvNet(TFL):
     def __init__(self, *args, **kwargs):
         self.num_channels = kwargs.get("num_channels", 1)
