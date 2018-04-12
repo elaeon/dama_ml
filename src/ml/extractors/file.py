7570d1532866ed18cb53e6c1ad34dd47b60b17d8
Alejandro Martinez Romero <mara80@gmail.com>
2018-04-12 14:45:31 -0500

    added to csv extractors support for zip files

--- src/ml/extractors/file.py
+++ src/ml/extractors/file.py
@@ -1,24 +1,74 @@
+#https://stackoverflow.com/questions/13044562/python-mechanism-to-identify-compressed-file-type-and-uncompress
 import csv
 import zipfile
+#import bz2
+#import gzip
 import StringIO
 
 
-class CSV(object):
-    def __init__(self, filepath, filename=None, delimiter=","):
-        self.filepath = filepath
-        self.delimiter = delimiter
-        self.filename = filename
+def get_compressed_file_manager(filepath):
+    with file(filepath, 'rb') as f:
+        start_of_file = f.read(1024)
+        f.seek(0)
+        for cls in (ZIPFile,):
+            if cls.is_magic(start_of_file):
+                return cls(filepath)
+        return File(filepath)
 
-    def reader(self, header=True, limit=None):
+
+def get_compressed_file_manager_ext(filepath):
+    ext = filepath.split(".").pop()
+    for cls in (File, ZIPFile):
+        if cls.proper_extension == ext:
+            return cls(filepath)
+
+
+class File(object):
+    magic = None
+    file_type = 'csv'
+    mime_type = 'text/plain'
+    proper_extension = 'csv'
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    @classmethod
+    def is_magic(self, data):
+        if self.magic is not None:
+            return data.startswith(self.magic)
+        return True
+
+    def reader(self, header=True, limit=None, filename=None, delimiter=","):
+        with open(self.filepath, 'r') as f:
+            csv_reader = csv.reader(f, delimiter=delimiter)
+            if header is False:
+                next(csv_reader)
+            for i, row in enumerate(csv_reader):
+                if limit is not None and i > limit:
+                    break
+                yield row
+
+    def writer(self, iterator, filename=None, delimiter=","):
+        with open(self.filepath, 'w') as f:
+            csv_writer = csv.writer(f, delimiter=delimiter)
+            for row in iterator:
+                csv_writer.writerow(row)
+
+
+class ZIPFile(File):
+    magic = '\x50\x4b\x03\x04'
+    file_type = 'zip'
+    mime_type = 'compressed/zip'
+    proper_extension = 'zip'
+
+    def reader(self, header=True, limit=None, filename=None, delimiter=","):
         with zipfile.ZipFile(self.filepath, 'r') as zf:
             files = zf.namelist()
             if len(files) == 1:
                 filename = files[0]
-            else:
-                filename = self.filename
 
             with zf.open(filename, 'r') as f:
-                csv_reader = csv.reader(f, delimiter=self.delimiter)
+                csv_reader = csv.reader(f, delimiter=delimiter)
                 if header is False:
                     next(csv_reader)
                 for i, row in enumerate(csv_reader):
@@ -26,11 +76,61 @@ class CSV(object):
                         break
                     yield row
 
-    def writer(self, iterator):
+    def writer(self, iterator, filename=None, delimiter=","):
         with zipfile.ZipFile(self.filepath, "w", zipfile.ZIP_DEFLATED) as zf:
             output = StringIO.StringIO()
-            csv_writer = csv.writer(output, delimiter=self.delimiter)
+            csv_writer = csv.writer(output, delimiter=delimiter)
             for row in iterator:
                 csv_writer.writerow(row)
-            zf.writestr(self.filename, output.getvalue())
+            zf.writestr(filename, output.getvalue())
             output.close()
+
+
+#class BZ2File (CompressedFile):
+#    magic = '\x42\x5a\x68'
+#    file_type = 'bz2'
+#    mime_type = 'compressed/bz2'
+
+#    def open(self):
+#        return bz2.BZ2File(self.f)
+
+
+#class GZFile (CompressedFile):
+#    magic = '\x1f\x8b\x08'
+#    file_type = 'gz'
+#    mime_type = 'compressed/gz'
+
+#    def open(self):
+#        return gzip.GzipFile(self.f)
+
+
+class CSV(object):
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def header(self, filename=None, delimiter=","):
+        return list(self.reader(limit=0, filename=filename, delimiter=delimiter))[0]
+
+    def shape(self, header=False, filename=None):
+        size = sum(1 for _ in self.reader(limit=None, header=header, filename=filename))
+        return size, len(self.header())
+
+    def reader(self, header=True, limit=None, filename=None, delimiter=","):
+        file_manager = get_compressed_file_manager(self.filepath)
+        if filename is None:
+            filename = self.filepath
+        return file_manager.reader(header=header, limit=limit, filename=filename,
+            delimiter=delimiter)
+    
+    def writer(self, iterator, filename=None, delimiter=","):
+        file_manager = get_compressed_file_manager_ext(self.filepath)
+        if filename is None:
+            filename = self.filepath
+        return file_manager.writer(iterator, filename=filename, 
+            delimiter=delimiter)
+
+    #def __iter__(self):
+    #    return self
+
+    #def next(self):
+    #    pass
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,19 +1,60 @@
 import unittest
 import zipfile
 
-from ml.extractors.file import CSV
+from ml.extractors.file import CSV, ZIPFile, File, get_compressed_file_manager_ext
+
+
+class TestCSVZip(unittest.TestCase):
+    def setUp(self):
+        self.iterator = [["A", "B", "C", "D", "F"], [1, 2, 3, 4, 5]]
+        self.filepath = "/tmp/test.zip"
+        self.filename = "test.csv"
+        csv_writer = CSV(filepath=self.filepath)
+        csv_writer.writer(self.iterator, filename=self.filename, delimiter=",")
+
+    def test_csv_ext(self):
+        fm = get_compressed_file_manager_ext(self.filepath)
+        self.assertEqual(fm.mime_type, ZIPFile.mime_type)
+
+    def test_csv(self):
+        csv = CSV(self.filepath)
+        for r0, r1 in zip(self.iterator, csv.reader(limit=1, filename=self.filename)):
+            self.assertItemsEqual(map(str, r0), r1)
+
+    def test_header(self):
+        csv = CSV(self.filepath)
+        self.assertItemsEqual(csv.header(), ["A", "B", "C", "D", "F"])
+
+    def test_shape(self):
+        csv = CSV(self.filepath)
+        self.assertItemsEqual(csv.shape(), (1, 5))
+        self.assertItemsEqual(csv.shape(header=True), (2, 5))
 
 
 class TestCSV(unittest.TestCase):
     def setUp(self):
         self.iterator = [["A", "B", "C", "D", "F"], [1, 2, 3, 4, 5]]
-        csv_writer = CSV(filepath="/tmp/test.zip", filename="test.csv")
-        csv_writer.writer(self.iterator)
+        self.filepath = "/tmp/test.csv"
+        csv_writer = CSV(filepath=self.filepath)
+        csv_writer.writer(self.iterator, delimiter=",")
+
+    def test_csv_ext(self):
+        fm = get_compressed_file_manager_ext(self.filepath)
+        self.assertEqual(fm.mime_type, File.mime_type)
 
     def test_csv(self):
-        csv = CSV("/tmp/test.zip")
+        csv = CSV(self.filepath)
         for r0, r1 in zip(self.iterator, csv.reader(limit=1)):
             self.assertItemsEqual(map(str, r0), r1)
+
+    def test_header(self):
+        csv = CSV(self.filepath)
+        self.assertItemsEqual(csv.header(), ["A", "B", "C", "D", "F"])
+
+    def test_shape(self):
+        csv = CSV(self.filepath)
+        self.assertItemsEqual(csv.shape(), (1, 5))
+        self.assertItemsEqual(csv.shape(header=True), (2, 5))
 
 
 if __name__ == '__main__':
